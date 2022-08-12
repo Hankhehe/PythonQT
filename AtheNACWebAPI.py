ccd5c26c9bd51baf31f37d0d0802aa6483238334
Hank <hz4154261@gmail.com>
2022-08-12 17:56:11 +0800

    fix

--- AtheNACWebAPI.py
+++ AtheNACWebAPI.py
@@ -1,4 +1,4 @@
-import requests,json
+import requests,json,ipaddress
 from urllib import parse
 
 class AthenacWebAPILibry :
@@ -21,25 +21,35 @@ class AthenacWebAPILibry :
         self.Token = 'Bearer '+ r['access_token']
         self.FreshToken ='Bearer ' + r['refresh_token']
     
-    def AddRange(self,mIP:str,gwIP:str) -> None :
+    def AddRange(self,mIP:str,gwIP:str,NetworkName:str) -> None :
         #需要提供 Networke ID
         #{"NetworkId": "14","Name": "192.168.99.X","ManagementIp": "192.168.99.89","StartIp": "192.168.99.1","EndIp": "192.168.99.254","GatewayIp": "192.168.99.254","SubnetMask": "255.255.255.0"}
+        IPrange = ipaddress.IPv4Network(mIP)
+        manageIP = str(mIP.split('/')[0])
+        submask = str(IPrange.netmask)
+        rangeName,first,last =str(IPrange[0]), str(IPrange[1]),str(IPrange[-2])
+        NetworkerId = self.GetNetworkInfoByName(NetworkName=NetworkName)
+        pass
         Path = '/api/IpRanges/V4'
+        Header = {'Authorization':self.Token,'Content-type': 'application/json'}
+        
+        Data = {}
 
     def AddNetwork(self) -> None :
         #需要提供 Probe ID
         #{"PixisProbeId": 10925416137,"NetworkName": "VLAnN999","VlanId": 999}
         Path = '/api/Networks/Create'
 
-    def GetNetworkInfoByName(self,Name:str)-> None :
+    def GetNetworkInfoByName(self,NetworkName:str)-> int | None :
         r = None
         Path = '/api/Networks/Query'
         Header = {'Authorization':self.Token,'Content-type': 'application/json'}
         Data = {'take':0}
         r = requests.post(self.ServerIP+Path,headers=Header,data=json.dumps(Data),verify=False)
         r = json.loads(r.text)['Data']
-        pass
-        # for i in r :
-        #     i['']
+        for i in r :
+            if i['NetworkName'] == NetworkName :
+                return i['NetworkId']
+            
 
     
--- TestConsole.py
+++ TestConsole.py
@@ -2,5 +2,7 @@ from AtheNACWebAPI import AthenacWebAPILibry
 import base64
 
 option = AthenacWebAPILibry('http://192.168.21.180:8000','admin',base64.b64encode('admin'.encode('UTF-8')))
-option.GetNetworkInfoByName('VLAN21')
+# NetworkID = option.GetNetworkInfoByName('VLAN 21')
+option.AddRange(mIP='192.168.98.0/23',gwIP='192.168.99.254',NetworkName='VLAN 21')
+pass
 
