4edd06009401ba109b45de69528c55c61c87ebc7
Hank <hz4514261@gmail.com>
2022-08-13 21:59:56 +0800

    fix

--- AtheNACWebAPI.py
+++ AtheNACWebAPI.py
@@ -22,8 +22,6 @@ class AthenacWebAPILibry :
         self.FreshToken ='Bearer ' + r['refresh_token']
     
     def AddRange(self,mIP:str,gwIP:str,NetworkName:str) -> None :
-        #需要提供 Networke ID
-        #{"NetworkId": "14","Name": "192.168.99.X","ManagementIp": "192.168.99.89","StartIp": "192.168.99.1","EndIp": "192.168.99.254","GatewayIp": "192.168.99.254","SubnetMask": "255.255.255.0"}
         manageIP = str(mIP.split('/')[0])
         networkeIP = str(ipaddress.ip_interface(mIP).network)
         IPrange = ipaddress.IPv4Network(networkeIP)
@@ -42,8 +40,6 @@ class AthenacWebAPILibry :
         requests.post(self.ServerIP+Path,headers=Header,data=json.dumps(Data),verify=False)
 
     def AddNetwork(self,ProbeID:int,NetworkName,VLANID:int) -> None :
-        #需要提供 Probe ID
-        #{"PixisProbeId": 10925416137,"NetworkName": "VLAnN999","VlanId": 999}
         Path = '/api/Networks/Create'
         Header = {'Authorization':self.Token,'Content-type': 'application/json'}
         Data = {"PixisProbeId": ProbeID,"NetworkName": NetworkName,"VlanId": VLANID}
--- TestConsole.py
+++ TestConsole.py
@@ -1,15 +1,20 @@
 from AtheNACWebAPI import AthenacWebAPILibry
-import base64
+import base64,csv
 
-ProbeID = 10925416137
-netName = 'VLAN 118'
-VLANId = 118
-manageIP = '192.168.118.171/24'
-gatewayIP = '192.168.118.254'
-
-option = AthenacWebAPILibry('http://192.168.21.180:8000','admin',base64.b64encode('admin'.encode('UTF-8')))
-ID = option.GetNetworkInfoByName('VLAN 118')
-option.AddNetwork(ProbeID=ProbeID,NetworkName=netName,VLANID=VLANId)
-option.AddRange(mIP=manageIP,gwIP=gatewayIP,NetworkName=netName)
+# ProbeID = 10925416137
+# netName = 'VLAN 118'
+# VLANId = 118
+# manageIP = '192.168.118.171/24'
+# gatewayIP = '192.168.118.254'
+Datas = list()
+with open('IPRange.csv',newline='') as f :
+    Rows = csv.reader(f)
+    for Row in Rows:
+        Datas.append(Row)
+del Datas[0]
+for Data in Datas:
+    option = AthenacWebAPILibry('http://192.168.21.180:8000','admin',base64.b64encode('admin'.encode('UTF-8')))
+    option.AddNetwork(ProbeID=Data[0],NetworkName=Data[1],VLANID=Data[2])
+    option.AddRange(mIP=Data[3],gwIP=Data[4],NetworkName=Data[1])
 pass
 
